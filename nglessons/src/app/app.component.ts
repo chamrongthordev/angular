a4f50fe6381df3bd1fd0c948e854d14a965a9e98
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-25 14:44:52 +0700

    Add example

--- nglessons/src/app/app.component.ts
+++ nglessons/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import {Component} from '@angular/core';
 
+interface UserModel {
+  name: string,
+  gender: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  searchTerm: string = '';
 
-  handleInput($event: string) {
-    this.searchTerm = $event;
-  }
+export class AppComponent {
+  users: UserModel[] = [
+    {
+      name: 'Chamrong',
+      gender: 'M'
+    },
+    {
+      name: 'Lam',
+      gender: 'F'
+    }
+  ];
 }
